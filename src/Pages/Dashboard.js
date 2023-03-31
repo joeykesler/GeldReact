c77ff3d257db5427fbe6774dc10044b5a076f7f5
rafael1347 <rafaelm_0495@hotmail.com>
2023-03-31 12:08:15 -0400

    Additions of a couple pages and moved some code

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar/Navbar";
+
+function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Login from "./Login";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Stocks from "./Stocks";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/stocks" element={<Stocks />} />
-      </Routes>
-     </BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
